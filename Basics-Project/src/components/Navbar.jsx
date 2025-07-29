f7dd67a2a9ee015154fd332af3298456e15dfc74
Johnson Bhaskaran <johnsonb@live.in>
2025-07-29 21:22:43 +0530

    Rendering List using map( ) Method

--- Basics-Project/src/components/Navbar.jsx
+++ Basics-Project/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 const Navbar = () => {
   let x = "John";
+  let usersArray = ["user 1", "user 2", "user 3", "user 4", "user 5"];
 
   return (
     <>
       Navbar
       <div>{x}</div>
+      <ol>
+        {usersArray.map((user, id) => {
+          return <li key={id}>{user}</li>;
+        })}
+      </ol>
     </>
   );
 };
