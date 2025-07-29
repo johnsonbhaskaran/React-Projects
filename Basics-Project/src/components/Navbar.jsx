9e4a8fec2b9429e03f027a435802e84bb3bf3b88
Johnson Bhaskaran <johnsonb@live.in>
2025-07-29 21:25:45 +0530

    Conditional Rendering using ternary operator

--- Basics-Project/src/components/Navbar.jsx
+++ Basics-Project/src/components/Navbar.jsx
@@ -1,18 +1,7 @@
 const Navbar = () => {
-  let x = "John";
-  let usersArray = ["user 1", "user 2", "user 3", "user 4", "user 5"];
+  let data = "boy";
 
-  return (
-    <>
-      Navbar
-      <div>{x}</div>
-      <ol>
-        {usersArray.map((user, id) => {
-          return <li key={id}>{user}</li>;
-        })}
-      </ol>
-    </>
-  );
+  return <>{data === "boy" ? <h1>Boy</h1> : <h1>Girl</h1>}</>;
 };
 
 export default Navbar;
