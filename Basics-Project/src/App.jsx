b26a33496a4bc7ba8a0d29aad7a5c430d32b9964
Johnson Bhaskaran <johnsonb@live.in>
2025-07-30 07:19:10 +0530

    Getting value of input field using useRef hook and store it Array to display.

--- Basics-Project/src/App.jsx
+++ Basics-Project/src/App.jsx
@@ -1,19 +1,27 @@
-import { useRef } from "react";
-import FirstComponent from "./components/FirstComponent";
+import { useRef, useState } from "react";
 
 const App = () => {
+  const [data, setData] = useState([]);
   const inputRef = useRef(null);
 
   return (
     <div>
       <input ref={inputRef} type='text' />
       <button
-        onClick={() => {
-          console.log(inputRef.current.value);
+        onClick={(e) => {
+          e.preventDefault();
+          setData([...data, inputRef.current.value]);
         }}
       >
         Submit
       </button>
+      {data.map((item, index) => {
+        return (
+          <h1 key={index}>
+            {index} {item}
+          </h1>
+        );
+      })}
     </div>
   );
 };
