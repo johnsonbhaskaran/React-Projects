d082bca1ef4da476682de4c230f510651cb42b8c
Johnson Bhaskaran <johnsonb@live.in>
2025-08-06 22:46:32 +0530

    Button component

--- CustomDropdown/src/App.jsx
+++ CustomDropdown/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <>
-      <div>APP</div>
-    </>
+    <div className='bg-gray-50 h-screen'>
+      <nav className='flex justify-end items-start px-4 py-2 border-b'>
+        <Button>Menu</Button>
+      </nav>
+    </div>
   );
 }
 
