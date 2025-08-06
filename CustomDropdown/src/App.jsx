91109f91c32e058c8fdea4b1fd6fe0fd597cd258
Johnson Bhaskaran <johnsonb@live.in>
2025-08-06 23:21:04 +0530

    Drop functionality completed along with UI

--- CustomDropdown/src/App.jsx
+++ CustomDropdown/src/App.jsx
@@ -9,16 +9,29 @@ function App() {
       <nav className='flex justify-end items-start shadow shadow-gray-900 px-4 py-2'>
         <Dropdown trigger={<Button>Menu</Button>}>
           <DropdownList>
-            <FeatherIcon icon='cart' /> Cart
+            <img
+              src='https://api.dicebear.com/9.x/adventurer/svg?seed=Amaya'
+              alt=''
+              className='rounded-full w-16'
+            />
+            <div className='py-2'>
+              <p className='font-medium'>John Doe</p>
+              <a href='' className='text-sm-font-medium text-gray-500'>
+                johndoe@gmail.com
+              </a>
+            </div>
           </DropdownList>
           <DropdownList>
-            <FeatherIcon icon='cart' /> Cart
+            <FeatherIcon icon='shopping-cart' /> Cart
           </DropdownList>
           <DropdownList>
-            <FeatherIcon icon='cart' /> Cart
+            <FeatherIcon icon='package' /> Orders
           </DropdownList>
           <DropdownList>
-            <FeatherIcon icon='cart' /> Cart
+            <FeatherIcon icon='settings' /> Preferences
+          </DropdownList>
+          <DropdownList>
+            <FeatherIcon icon='log-out' /> Logout
           </DropdownList>
         </Dropdown>
       </nav>
