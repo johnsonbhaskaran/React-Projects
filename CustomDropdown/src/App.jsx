7e584cb9149c15cb67746bf430b8460d1d9c5b74
Johnson Bhaskaran <johnsonb@live.in>
2025-08-06 22:52:58 +0530

    Simple Button component added

--- CustomDropdown/src/App.jsx
+++ CustomDropdown/src/App.jsx
@@ -3,8 +3,8 @@ import Button from "./components/Button";
 
 function App() {
   return (
-    <div className='bg-gray-50 h-screen'>
-      <nav className='flex justify-end items-start px-4 py-2 border-b'>
+    <div className='bg-gray-950 h-screen text-white'>
+      <nav className='flex justify-end shadow shadow-gray-900 px-4 py-2'>
         <Button>Menu</Button>
       </nav>
     </div>
--- CustomDropdown/src/components/Button.jsx
+++ CustomDropdown/src/components/Button.jsx
@@ -1,4 +1,8 @@
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ children }) => {
+  return (
+    <button className='flex bg-blue-600 px-4 py-2 rounded-lg font-medium text-white'>
+      {children}
+    </button>
+  );
 };
 export default Button;
