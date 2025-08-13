4d42effb1cf06c85ae559dc8694eef8870bd3618
Johnson Bhaskaran <johnsonb@live.in>
2025-08-13 07:52:35 +0530

    Installed Redux toolkit package

--- Redux_Toolkit-CounterApp/src/App.jsx
+++ Redux_Toolkit-CounterApp/src/App.jsx
@@ -3,11 +3,21 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className='flex justify-center items-center bg-slate-900 h-screen'>
-        <div className='flex bg-'>
-          <button>-</button>
+      <div className='flex justify-center items-center bg-gray-950 h-screen text-white'>
+        <div className='flex items-center gap-4 bg-violet-500 p-4 rounded-lg text-5xl'>
+          <button
+            className='bg-violet-600 hover:bg-white rounded-full size-12 hover:text-violet-600 cursor-pointer'
+            id='decrease'
+          >
+            -
+          </button>
           <div className='count'>0</div>
-          <button id='increase'>+</button>
+          <button
+            className='bg-violet-600 hover:bg-white rounded-full size-12 hover:text-violet-600 cursor-pointer'
+            id='increase'
+          >
+            +
+          </button>
         </div>
       </div>
     </>
