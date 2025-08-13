592ee42ced168e5233829be21e0a3834c5e6be65
Johnson Bhaskaran <johnsonb@live.in>
2025-08-13 07:36:22 +0530

    React vite Boilerplate updated with tailwind package

--- Redux_Toolkit-CounterApp/src/App.jsx
+++ Redux_Toolkit-CounterApp/src/App.jsx
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>APP</div>
+      <div className='flex justify-center items-center bg-slate-900 h-screen'>
+        <div className='flex bg-'>
+          <button>-</button>
+          <div className='count'>0</div>
+          <button id='increase'>+</button>
+        </div>
+      </div>
     </>
   );
 }
