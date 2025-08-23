162dce6a8ce7bc173b1a03c48c39171921392c00
Johnson Bhaskaran <johnsonb@live.in>
2025-08-23 21:28:34 +0530

    rmdir

--- React_Redux-FakeShop/src/App.jsx
+++ React_Redux-FakeShop/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>APP</div>
+      <h1>App</h1>
     </>
   );
 }
--- React_Redux-FakeShop/src/main.jsx
+++ React_Redux-FakeShop/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./redux/store.js";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
