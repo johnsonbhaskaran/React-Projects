c0124ecca471e36ead26dc964e6829b1cbbd8c5a
Johnson Bhaskaran <johnsonb@live.in>
2025-09-01 21:24:24 +0530

    App jsx

--- React-Form_Validation/src/App.jsx
+++ React-Form_Validation/src/App.jsx
@@ -4,7 +4,38 @@ import LoginForm from "./components/LoginForm";
 function App() {
   return (
     <>
-      <LoginForm />
+      <section className='section'>
+        <div className='container'>
+          <h1 className='has-text-primary title'>Hello World</h1>
+          <p className='subtitle'>
+            My first website with <strong>Bulma</strong>!
+          </p>
+        </div>
+      </section>
+      <section className='section'>
+        <div className='container'>
+          <h2 className='subtitle'>Columns</h2>
+          <LoginForm />
+        </div>
+      </section>
+      <section className='section'>
+        <div className='container'>
+          <div className='skeleton-block'></div>
+          <hr className='divider'></hr>
+          <div className='skeleton-lines'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className='container'>
+            <div className='buttons'>
+              <button className='button is-skeleton'>Button</button>
+              <button className='button is-link is-skeleton'>Link</button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
