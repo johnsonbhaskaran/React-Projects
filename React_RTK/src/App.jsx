df826d99a4da484be79913d27bdb6247916904f3
Johnson Bhaskaran <johnsonb@live.in>
2025-09-04 17:30:00 +0530

    React RTK - counter with increment by input amount

--- React_RTK/src/App.jsx
+++ React_RTK/src/App.jsx
@@ -1,42 +1,46 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement, reset, incrementByAmount } from "./features/counter/counterSlice.js";
+import { increment, decrement, reset, incrementByAmount } from "./features/counter/counterSlice";
 import { useState } from "react";
 
 function App() {
-  const count = useSelector((state) => state.counter.value);
+  const { count } = useSelector((state) => state.counter);
   const dispatch = useDispatch();
-  const [inpValue, setInpValue] = useState(0);
+  const [value, setValue] = useState(0);
+
+  const valueChangeHandler = (v) => {
+    setValue(v);
+  };
 
   return (
     <div className='flex justify-center items-center bg-indigo-950 h-screen text-white'>
-      <div className='flex flex-col space-y-5'>
+      <div className='flex flex-col space-y-6 p-2'>
         <h1 className='font-bold text-3xl text-center'>Counter</h1>
-        <div className='text-center'>
-          <p>Counter value: {count}</p>
-        </div>
+        <p className='text-center'>Value: {count}</p>
         <div className='space-x-3'>
-          <button className='px-3 py-1 border rounded-sm' onClick={() => dispatch(decrement())}>
+          <button className='px-3 py-1 border rounded' onClick={() => dispatch(decrement())}>
             decrement
           </button>
-          <button className='px-3 py-1 border rounded-sm' onClick={() => dispatch(reset())}>
+          <button className='px-3 py-1 border rounded' onClick={() => dispatch(reset())}>
             reset
           </button>
-          <button className='px-3 py-1 border rounded-sm' onClick={() => dispatch(increment())}>
+          <button className='px-3 py-1 border rounded' onClick={() => dispatch(increment())}>
             increment
           </button>
         </div>
-        <div className='flex justify-between space-x-2'>
+        <div className='flex justify-between space-x-3'>
           <input
             type='text'
-            className='border-b focus:outline-0'
-            value={inpValue}
-            onChange={(e) => setInpValue(Number(e.target.value))}
-            placeholder='Enter counter value'
+            placeholder='Enter the value..'
+            className='border-b focus:outline-none text-white/30 focus:text-white'
+            value={value}
+            onChange={(e) => {
+              valueChangeHandler(Number(e.target.value));
+            }}
           />
           <button
-            className='px-3 py-1 border rounded-sm'
-            onClick={() => dispatch(incrementByAmount(inpValue))}
+            className='px-3 py-1 border rounded'
+            onClick={() => dispatch(incrementByAmount(value))}
           >
             Add this
           </button>
--- React_RTK/src/features/counter/counterSlice.js
+++ React_RTK/src/features/counter/counterSlice.js
@@ -1,24 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  value: 0,
-};
-
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: { count: 0 },
   reducers: {
     increment: (state) => {
-      state.value += 1;
+      state.count += 1;
     },
     reset: (state) => {
-      state.value = 0;
+      state.count = 0;
     },
     decrement: (state) => {
-      state.value -= 1;
+      state.count -= 1;
     },
     incrementByAmount: (state, action) => {
-      state.value = state.value + action.payload;
+      state.count += action.payload;
     },
   },
 });
