fb973c99c88e5dc9717f1923b7f17247ca18f722
Johnson Bhaskaran <johnsonb@live.in>
2025-09-05 18:56:47 +0530

    React redux toolkit api CRUD mechanism with createAsyncThunk

--- React_RTK_createAsyncThunk/src/App.jsx
+++ React_RTK_createAsyncThunk/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
-import { addUser, fetchUsers, removeUser, updateUser } from "./features/users/usersSlice.js";
+import { fetchUsers, addUser, deleteUser, updateUser } from "./features/users/usersSlice.js";
 
 function App() {
-  const { list, loading, error } = useSelector((state) => state.users);
   const dispatch = useDispatch();
+  const { list, loading, error } = useSelector((state) => state.users);
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [editId, setEditId] = useState(null);
@@ -16,6 +17,7 @@ function App() {
 
   const handleAddOrUpdateUser = () => {
     if (!name || !email) return;
+
     if (editId) {
       dispatch(updateUser({ id: editId, name, email }));
       setEditId(null);
@@ -26,8 +28,8 @@ function App() {
     setEmail("");
   };
 
-  const handleRemoveUser = (id) => {
-    dispatch(removeUser(id));
+  const handleDeleteUser = (id) => {
+    dispatch(deleteUser(id));
   };
 
   const handleEditUser = (user) => {
@@ -42,55 +44,57 @@ function App() {
         <h1 className='mr-6 mb-6 text-3xl'>Users</h1>
 
         {loading && <p className='text-blue-500'>Loading...</p>}
-        {error && <p className='text-red-500'>Error: {error}</p>}
+        {error && <p className='text-red-600'>Error: {error} </p>}
       </div>
 
       <div className='space-x-4 bg-slate-900 mb-6 p-5 border border-slate-700 rounded'>
         <input
-          onChange={(e) => setName(e.target.value)}
           type='text'
           value={name}
-          placeholder='enter name'
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          placeholder='Enter name'
           className='p-2 border border-slate-700 focus:outline-none'
         />
         <input
-          onChange={(e) => setEmail(e.target.value)}
           type='email'
           value={email}
-          placeholder='enter email'
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder='Enter email'
           className='p-2 border border-slate-700 focus:outline-none'
         />
         <button
           onClick={handleAddOrUpdateUser}
-          className='hover:bg-white/70 px-5 py-2 border rounded-full hover:text-slate-950 transition-all duration-300 cursor-pointer'
+          className='hover:bg-white/70 p-2 px-5 border rounded-full hover:text-slate-950 cursor-pointer'
         >
-          {!editId ? `Add User ➕` : `Update User ✏️`}
+          {editId ? `Update User ✏️` : `Add User ➕`}
         </button>
       </div>
 
-      <ul className='space-y-3'>
+      <ul className='space-y-4'>
         {list.map((user) => (
-          <li key={user.id} className='flex justify-between p-2 border rounded'>
-            <span>
-              {user.name} ({user.email})
-            </span>
-            <div className='space-x-2'>
-              <button
-                onClick={() => {
-                  handleEditUser(user);
-                }}
-                className='hover:bg-emerald-600 px-2 border rounded-full cursor-pointer'
-              >
-                ✅
-              </button>
-              <button
-                onClick={() => {
-                  handleRemoveUser(user.id);
-                }}
-                className='hover:bg-red-600 px-2 border rounded-full cursor-pointer'
-              >
-                ❌
-              </button>
+          <li key={user.id} className='hover:bg-white/10 p-2 border rounded'>
+            <div className='flex justify-between'>
+              <span>
+                {user.name} ({user.email})
+              </span>
+              <div className='flex space-x-2'>
+                <button
+                  onClick={() => handleEditUser(user)}
+                  className='hover:bg-amber-300 px-2 border rounded-full cursor-pointer'
+                >
+                  ✅
+                </button>
+                <button
+                  onClick={() => handleDeleteUser(user.id)}
+                  className='hover:bg-amber-300 px-2 border rounded-full cursor-pointer'
+                >
+                  ❌
+                </button>
+              </div>
             </div>
           </li>
         ))}
--- React_RTK_createAsyncThunk/src/features/users/usersSlice.js
+++ React_RTK_createAsyncThunk/src/features/users/usersSlice.js
@@ -1,29 +1,28 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// GET all users
+// 📌 GET all users
 export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
   const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-  return response.data; //! createAsyncThunk returns it as action.payload
+  return response.data;
 });
 
-// POST new user
+// 📌 POST new user
 export const addUser = createAsyncThunk("users/addUser", async (newUser) => {
   const response = await axios.post("https://jsonplaceholder.typicode.com/users", newUser);
   return response.data;
 });
 
-// DELETE user
-export const removeUser = createAsyncThunk("users/removeUser", async (id) => {
+// 📌 DELETE user
+export const deleteUser = createAsyncThunk("users/deleteUser", async (id) => {
   await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
-  return id; //! Returns ID so we can remove from state
+  return id;
 });
-
-// UPDATE user
+// 📌 UPDATE user
 export const updateUser = createAsyncThunk("users/updateUser", async (updatedUser) => {
   const { id, ...data } = updatedUser;
   const response = await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, data);
-  return response.data; //! Returns updated user
+  return response.data;
 });
 
 const usersSlice = createSlice({
@@ -35,10 +34,11 @@ const usersSlice = createSlice({
   },
   reducers: {},
   extraReducers: (builder) => {
-    // 📌 GET users
+    // GET Users
     builder
       .addCase(fetchUsers.pending, (state) => {
         state.loading = true;
+        state.error = null;
       })
       .addCase(fetchUsers.fulfilled, (state, action) => {
         state.loading = false;
@@ -46,59 +46,24 @@ const usersSlice = createSlice({
       })
       .addCase(fetchUsers.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.payload.error;
+        state.error = action.error.message;
       });
 
-    // 📌 POST users
-    builder
-      .addCase(addUser.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(addUser.fulfilled, (state, action) => {
-        state.loading = false;
-        state.list.push(action.payload);
-      })
-      .addCase(addUser.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload.error;
-      });
+    // POST new user
+    builder.addCase(addUser.fulfilled, (state, action) => {
+      state.list.push(action.payload);
+    });
 
-    // 📌 DELETE user
-    builder
-      .addCase(removeUser.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(removeUser.fulfilled, (state, action) => {
-        state.loading = false;
-        state.list = state.list.filter((user) => user.id !== action.payload);
-      })
-      .addCase(removeUser.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload.error;
-      });
+    // DELETE user
+    builder.addCase(deleteUser.fulfilled, (state, action) => {
+      state.list = state.list.filter((user) => user.id !== action.payload);
+    });
 
-    // 📌 UPDATE user
-    builder
-      .addCase(updateUser.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(updateUser.fulfilled, (state, action) => {
-        const index = state.list.findIndex((u) => u.id === action.payload.id);
-        if (index !== -1) state.list[index] = action.payload;
-        state.loading = false;
-
-        // state.list = () =>
-        //   state.list.map((user) => {
-        //     if (user.id !== action.payload) {
-        //       user.name = action.payload.name;
-        //       user.email = action.payload.email;
-        //     }
-        //   });
-      })
-      .addCase(updateUser.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload.error;
-      });
+    //UPDATE user
+    builder.addCase(updateUser.fulfilled, (state, action) => {
+      const index = state.list.findIndex((user) => user.id === action.payload.id);
+      state.list[index] = action.payload;
+    });
   },
 });
 
