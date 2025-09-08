d4da46334fb341e12f64973825c70ee7f957e9ba
Johnson Bhaskaran <johnsonb@live.in>
2025-09-09 05:08:08 +0530

    Listing component created

--- React_RTK_OMDB_API/src/components/MovieListing.jsx
+++ React_RTK_OMDB_API/src/components/MovieListing.jsx
@@ -1,10 +1,28 @@
 import { useSelector } from "react-redux";
 import { getAllMovies } from "../features/movies/movieSlice";
+import MovieCard from "./MovieCard.jsx";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
+  let renderMovies = "";
+
+  renderMovies =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => {
+        <MovieCard key={index} data={movie} />;
+      })
+    ) : (
+      <div className='text-red-500'>{movies.Error}</div>
+    );
 
   console.log(movies);
-  return <div>MovieListing</div>;
+  return (
+    <div>
+      <div>
+        <h2>Dovies</h2>
+        <div>{renderMovies}</div>
+      </div>
+    </div>
+  );
 };
 export default MovieListing;
