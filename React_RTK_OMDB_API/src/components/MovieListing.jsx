860bcb0abaec83c6c7bf6bab89835a5ac51ed8f5
Johnson Bhaskaran <johnsonb@live.in>
2025-09-09 04:59:38 +0530

    rectified the incorrectly configured the store

--- React_RTK_OMDB_API/src/components/MovieListing.jsx
+++ React_RTK_OMDB_API/src/components/MovieListing.jsx
@@ -1,4 +1,10 @@
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../features/movies/movieSlice";
+
 const MovieListing = () => {
+  const movies = useSelector(getAllMovies);
+
+  console.log(movies);
   return <div>MovieListing</div>;
 };
 export default MovieListing;
