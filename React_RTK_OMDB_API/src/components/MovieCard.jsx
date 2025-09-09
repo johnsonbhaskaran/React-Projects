8d9868150e80344036ef4d2ab735967722037c42
Johnson Bhaskaran <johnsonb@live.in>
2025-09-09 18:41:42 +0530

    Movie card styled

--- React_RTK_OMDB_API/src/components/MovieCard.jsx
+++ React_RTK_OMDB_API/src/components/MovieCard.jsx
@@ -1,4 +1,16 @@
-const MovieCard = () => {
-  return <div>moviecard</div>;
+const MovieCard = ({ data }) => {
+  return (
+    <div className='bg-slate-700'>
+      <div className=''>
+        <div className=''>
+          <img className='w-full' src={data.Poster} alt={data.Title} />
+        </div>
+        <div className='p-5'>
+          <h4 className='relative text-xl'>{data.Title}</h4>
+          <p>{data.Year}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default MovieCard;
