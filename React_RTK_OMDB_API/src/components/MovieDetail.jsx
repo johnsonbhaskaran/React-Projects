e4db85ac2df55081b639bf4d3f2d0cfd0993508e
Johnson Bhaskaran <johnsonb@live.in>
2025-09-09 22:24:05 +0530

    Addition: added all the necessary details

--- React_RTK_OMDB_API/src/components/MovieDetail.jsx
+++ React_RTK_OMDB_API/src/components/MovieDetail.jsx
@@ -10,18 +10,36 @@ const MovieDetail = () => {
   const { imdbID } = useParams();
   const dispatch = useDispatch();
   const data = useSelector(getMovieOrSeriesDetail);
-  console.log(data);
 
   useEffect(() => {
     dispatch(fetchMovieOrSeriesDetailAsync(imdbID));
   }, [dispatch, imdbID]);
 
   return (
-    <div>
-      <h3>Movie Details</h3>
-      <div>{data.Title}</div>
-      <img src={data.Poster} alt='' />
-    </div>
+    <>
+      <div className='flex justify-center my-6'>
+        <div className='space-y-6 mr-6 w-1/2'>
+          <h2 className='text-5xl'>{data.Title}</h2>
+          <div className='flex gap-6'>
+            <p>IMDB Rating: {data.imdbRating}</p>
+            <p>IMDB Votes: {data.imdbVotes}</p>
+            <p>Runtime: {data.Runtime}</p>
+            <p>Year: {data.Year}</p>
+          </div>
+          <div className='space-y-3'>
+            <p>{data.Plot}</p>
+            <p>Director: {data.Director}</p>
+            <p>Stars: {data.Actors}</p>
+            <p>Genres: {data.Genre}</p>
+            <p>Languages: {data.Language}</p>
+            <p>Awards: {data.Awards}</p>
+          </div>
+        </div>
+        <div className='w-fit'>
+          <img src={data.Poster} alt={data.Title} />
+        </div>
+      </div>
+    </>
   );
 };
 export default MovieDetail;
