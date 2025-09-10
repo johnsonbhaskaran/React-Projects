1978b4162e1fc0787f8e119be1359f3086135d9a
Johnson Bhaskaran <johnsonb@live.in>
2025-09-10 08:30:45 +0530

    Slick settings extracted to commons folder

--- React_RTK_OMDB_API/src/components/MovieListing.jsx
+++ React_RTK_OMDB_API/src/components/MovieListing.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { getAllMovies } from "../features/movies/movieSlice.js";
 import Slider from "react-slick";
 import MovieCard from "./MovieCard";
+import { settings } from "../common/slickSettings.js";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
@@ -13,40 +14,6 @@ const MovieListing = () => {
     ) : (
       <div className='text-red-500'>Error: {movies.Error}</div>
     );
-
-  let settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
 
   return (
     <div>
