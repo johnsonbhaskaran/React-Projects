f1b598f6bd48347a77d981fc19233565bc26ec7b
Johnson Bhaskaran <johnsonb@live.in>
2025-09-30 15:31:14 +0530

    Memoizing component with React Memo

---
+++ use_memo-React_memo/src/components/components/ExpensiveComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ExpensiveComponent = React.memo(() => {
+  console.log("expensive component rendered!");
+
+  let total = 0;
+  for (let i = 1; i <= 1000000000; i++) {
+    total = total + i;
+  }
+
+  return <div>ExpensiveComponent</div>;
+});
+export default ExpensiveComponent;
