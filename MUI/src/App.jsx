f72fa08b9e47732f9dbdc9bd938dfb65873b7540
Johnson Bhaskaran <johnsonb@live.in>
2025-10-01 20:24:35 +0530

    Done: Home page assembles all other components completed

--- MUI/src/App.jsx
+++ MUI/src/App.jsx
@@ -1,5 +1,20 @@
+import Feed from "./components/Feed";
+import RightBar from "./components/RightBar";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import { Box, Stack } from "@mui/system";
+
 function App() {
-  return <></>;
+  return (
+    <Box>
+      <Navbar />
+      <Stack direction='row' spacing={2} justifyContent='space-evenly'>
+        <Sidebar />
+        <Feed />
+        <RightBar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
