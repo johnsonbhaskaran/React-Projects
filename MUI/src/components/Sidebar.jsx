3d1038cdb20e0d1f59077188a859a502b0ee5b3c
Johnson Bhaskaran <johnsonb@live.in>
2025-10-01 09:58:53 +0530

    Done: sidebar with nav list items completed

---
+++ MUI/src/components/Sidebar.jsx
@@ -0,0 +1,94 @@
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { Box } from "@mui/system";
+import {
+  AccountBox,
+  Group,
+  Home,
+  ModeNight,
+  Pages,
+  People,
+  PeopleAlt,
+  Person,
+  Settings,
+  Shop,
+  Shop2,
+} from "@mui/icons-material";
+import Switch from "@mui/material/Switch";
+
+const Sidebar = () => {
+  return (
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#home'>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary='Homepage' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#home'>
+            <ListItemIcon>
+              <Pages />
+            </ListItemIcon>
+            <ListItemText primary='Pages' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#home'>
+            <ListItemIcon>
+              <Group />
+            </ListItemIcon>
+            <ListItemText primary='Groups' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#home'>
+            <ListItemIcon>
+              <Shop />
+            </ListItemIcon>
+            <ListItemText primary='Marketplace' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#home'>
+            <ListItemIcon>
+              <Person />
+            </ListItemIcon>
+            <ListItemText primary='Friends' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#home'>
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary='Settings' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <AccountBox />
+            </ListItemIcon>
+            <ListItemText primary='Profile' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <ModeNight />
+            </ListItemIcon>
+            <Switch />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
+export default Sidebar;
