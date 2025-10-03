1e2ac337dbf5df58d8abc62eb88372675dd51313
Johnson Bhaskaran <johnsonb@live.in>
2025-10-04 00:01:32 +0530

    New: Form component and css styling

--- React Forms - WO library/src/App.jsx
+++ React Forms - WO library/src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import FormInput from "./components/FormInput";
 
 function App() {
   return (
     <>
-      <div>APP</div>
+      <div className='app'>
+        <form action=''>
+          <FormInput placeholder={"username"} />
+          <FormInput placeholder={"email"} />
+          <FormInput placeholder={"fullname"} />
+          <FormInput placeholder={"password"} />
+        </form>
+      </div>
     </>
   );
 }
---
+++ React Forms - WO library/src/components/FormInput.jsx
@@ -0,0 +1,11 @@
+import "./formInput.css";
+
+const FormInput = (props) => {
+  return (
+    <div className='formInput'>
+      <label>Username</label>
+      <input type='text' placeholder={props.placeholder} />
+    </div>
+  );
+};
+export default FormInput;
