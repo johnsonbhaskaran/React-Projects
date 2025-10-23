2beae36acfcd7c602a27691d49918a0c70906736
Johnson Bhaskaran <johnsonb@live.in>
2025-10-23 20:11:40 +0530

    Done: added marker to the map view on the specified long lat

--- Mapbox/src/Components/Map.jsx
+++ Mapbox/src/Components/Map.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
@@ -11,6 +12,10 @@ export default function Map() {
       container: mapRef.current,
       center: [-0.127758, 51.507351], //[long, lat]
       zoom: 14,
+    });
+
+    map.on("load", () => {
+      new mapboxgl.Marker().setLngLat([-0.127758, 51.507351]).addTo(map);
     });
 
     return () => {
