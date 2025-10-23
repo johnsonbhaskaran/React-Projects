2fc61122da67a9df460d6f7b1419669b625e41b7
Johnson Bhaskaran <johnsonb@live.in>
2025-10-23 17:23:05 +0530

    Done: Map info accessed with mapbox-gl and displayed in page using useRef

--- Mapbox/src/Components/Map.jsx
+++ Mapbox/src/Components/Map.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
-import { useRef } from "react";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
 export default function Map() {
   const mapRef = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapRef.current,
+      center: [-0.127758, 51.507351], //[long, lat]
+      zoom: 14,
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return <div ref={mapRef} style={{ width: "100dvw", height: "100vh" }} />;
 }
