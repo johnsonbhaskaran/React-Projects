098eaa57c8cae2fdf5a3dd106d761f1ab651a282
Johnson Bhaskaran <johnsonb@live.in>
2025-10-28 18:37:03 +0530

    Done: simple home, about and blog with just text representing the page

---
+++ toggle-theme/src/pages/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className='page'>
+      <h1>About Page</h1>
+      <p>Welcome to Aboutpage.</p>
+    </div>
+  );
+};
+export default About;
---
+++ toggle-theme/src/pages/Blog.jsx
@@ -0,0 +1,9 @@
+const Blog = () => {
+  return (
+    <div className='page'>
+      <h1>Blog Page</h1>
+      <p>Welcome to Blogpage.</p>
+    </div>
+  );
+};
+export default Blog;
