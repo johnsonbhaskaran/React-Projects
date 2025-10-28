fe863023c41c123ab28b2440bc035669e70c1ed3
Johnson Bhaskaran <johnsonb@live.in>
2025-10-28 18:38:56 +0530

    Done: Routes declared

--- toggle-theme/src/App.jsx
+++ toggle-theme/src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <h1>Toggle theme</h1>
-    </>
+    <BrowserRouter>
+      {/* <h1>Toggle theme</h1> */}
+      {/* navlinks */}
+      <Navbar />
+      {/* routes */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/blog' element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
