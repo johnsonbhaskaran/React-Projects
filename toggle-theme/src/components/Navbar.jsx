994a154c4efb44e9d9f82588050d0ccec1cd9cd0
Johnson Bhaskaran <johnsonb@live.in>
2025-10-29 17:18:36 +0530

    Done: using custom hook useTheme the theme state and toggleTheme function context pulled in. There is button which toggles the state

--- toggle-theme/src/components/Navbar.jsx
+++ toggle-theme/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../theme-context";
 
 const Navbar = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <nav className='navbar'>
       <div>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
         <Link to='/blog'>Blog</Link>
+        <button onClick={toggleTheme}>{theme === "dark" ? "dark mode" : "light mode"}</button>
       </div>
     </nav>
   );
