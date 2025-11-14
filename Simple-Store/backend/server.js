d00d17617fbdcefc66b781466d1143a3041c04d4
Johnson Bhaskaran <johnsonb@live.in>
2025-11-14 22:00:34 +0530

    Done: Add product route created

--- Simple-Store/backend/server.js
+++ Simple-Store/backend/server.js
@@ -9,8 +9,22 @@ const PORT = process.env.PORT || 8000;
 * app Routes *
 /------------------------------------------------------------------*/
 
-app.get("/products", (req, res) => {
-  res.send("🚀 Reached products route");
+app.get("/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(200).json({ success: true, data: newProduct });
+  } catch (err) {
+    console.error("⚠️ Error in creating product:", err.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 /* -----------------------------------------------------------------/
