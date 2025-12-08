981c8c7da7ca3fe8dc59763adfe864f3fbf2d9a5
Johnson Bhaskaran <johnsonb@live.in>
2025-12-08 18:11:55 +0530

    Done: focus input field using useRef hook

--- React-Hooks-Simplified/src/Hooks/UseRefHook.jsx
+++ React-Hooks-Simplified/src/Hooks/UseRefHook.jsx
@@ -3,20 +3,29 @@ import { useEffect, useRef, useState } from "react";
 const UseRefHook = () => {
   const [name, setName] = useState("");
   const renderCount = useRef(1);
+  const inputRef = useRef();
 
   useEffect(() => {
     renderCount.current = renderCount.current + 1;
   });
 
+  function focus() {
+    inputRef.current.focus();
+  }
+
   return (
     <>
       <input
+        ref={inputRef}
         value={name}
         onChange={(e) => setName(e.target.value)}
         className='my-6 px-5 py-3 border rounded-md'
       />
       <div className='text-3xl'>My name is {name}</div>
       <div className='text-xl'>render count: {renderCount.current}</div>
+      <button onClick={focus} className='my-6 px-5 py-3 border rounded-md cursor-pointer'>
+        Focus
+      </button>
     </>
   );
 };
